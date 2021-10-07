acbd84463e790c018f3efbf47d7c06e17ee0b2ad
GordosTower <92114582+GordosTower@users.noreply.github.com>
2021-10-07 20:59:20 +0300

    Tower

---
+++ Trynagle/Trynagle/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trynagle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = int.Parse(Console.ReadLine());
+
+               for (int i = 0; i <= a; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    Console.Write("SURMAN " );
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
